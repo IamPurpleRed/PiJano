4db5ad45d960473c160e7102c388daa9b4536ed5
Ben Chen <boyben001@gmail.com>
2021-05-27 16:16:35 +0800

    修正按鈕沒有出現 //TODO: 方塊動不了，sheet.txt讀不到

--- src/HomePage.java
+++ src/HomePage.java
@@ -4,27 +4,28 @@ import java.awt.event.*;
 import javax.imageio.*;
 
 public class HomePage extends JFrame {
-
-    GamingPage frame;
+    Dimension frame_size = new Dimension(1600, 1000);
+    static Dimension user_size = Toolkit.getDefaultToolkit().getScreenSize();
     BetaStart start;
+    MainFrame mainFrame;
     JButton button;
     JFrame close = this;
 
     HomePage() {
         start = new BetaStart();
         this.setTitle("Beta");
-        this.setSize(1600, 1000);
+        this.setSize(frame_size);
         this.setResizable(false); // 固定視窗大小
-        //this.setLocation(screen_Size.width / 2 - this.getSize().width / 2, screen_Size.height / 2 - this.getSize().height / 2);
-        this.setLocation(150, 0);
-        ImageIcon image = new ImageIcon("res/logo.jpg"); // create a image icon
+        this.setLocation(user_size.width / 2 - frame_size.width / 2, user_size.height / 2 - frame_size.height / 2); // 置中
+        ImageIcon image = new ImageIcon("src/img/logo.png"); // create a image icon
         this.setIconImage(image.getImage()); // change frame's icon
         this.getContentPane().setBackground(Color.black); //背景顏色(消除panel邊框)
         this.getContentPane().setLayout(new BorderLayout());
-        //this.add(panel, BorderLayout.CENTER);
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE); // 關閉視窗即退出程式
+
         button = new JButton();
         try {
-            Image img = ImageIO.read(getClass().getResource("src/img/roller.png"));
+            Image img = ImageIO.read(getClass().getResource("img/roller.png"));
             button.setIcon(new ImageIcon(img));
             button.setOpaque(false); //透明化
             button.setContentAreaFilled(false); //button整個都透明
@@ -34,16 +35,14 @@ public class HomePage extends JFrame {
         } catch (Exception ex) {
             System.out.println(ex);
         }
+
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                frame = new GamingPage();
+                mainFrame = new MainFrame();
                 close.dispose();
-                frame.setVisible(true);
-
+                mainFrame.setVisible(true);
             }
-
         });
-
         this.add(button);
         this.add(start, BorderLayout.CENTER);
 
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,13 +6,12 @@ public class MainFrame extends JFrame {
     static Dimension user_size = Toolkit.getDefaultToolkit().getScreenSize();
 
     MainFrame() {
-        this.setTitle("Beta");  // TODO: 缺一個酷名字
+        this.setTitle("Beta"); // TODO: 缺一個酷名字
         this.setSize(frame_size);
-        this.setLocation(user_size.width / 2 - frame_size.width / 2,user_size.height / 2 - frame_size.height / 2); // 置中
-        this.getContentPane().add(new GamingPage());  // TODO: 之後要切成主畫面
+        this.setLocation(user_size.width / 2 - frame_size.width / 2, user_size.height / 2 - frame_size.height / 2); // 置中
+        this.getContentPane().add(new GamingPage()); // TODO: 之後要切成主畫面
         ImageIcon image = new ImageIcon("src/img/logo.png"); // create a image icon
         this.setIconImage(image.getImage()); // change frame's icon
-
         this.setVisible(true);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE); // 關閉視窗即退出程式
     }
